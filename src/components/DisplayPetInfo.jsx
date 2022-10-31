157675c6e34a283a453a729d8a31c4edf620b7cc
sjodave <sjodave@gmail.com>
2022-10-31 13:40:28 +0530

    commit 1

---
+++ src/components/DisplayPetInfo.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function DisplayPetInfo({ pet }) {
+  if (!pet) return;
+  console.log(pet);
+
+  return (
+    <div className="d-flex justify-content-center row m-5">
+      <div className="card col-md-8 col-sm-12">
+        <img
+          className="card-img-top"
+          src={pet?.image?.url}
+          alt="Card img cap"
+        />
+        <div className="card-body">
+          <h3 className="card-title">Name: {pet.name}</h3>
+          {pet.description ?? (
+            <p className="card-text">
+              <b>Description: </b>
+              {pet.description ? pet.description : "Not Available"}
+            </p>
+          )}
+          <p className="card-text">
+            <b>origin: </b>
+            {pet.origin ? pet.origin : "Not Available"}
+          </p>
+          <p className="card-text">
+            <b>life_span : </b>
+            {pet["life_span"]}
+          </p>
+          <p className="card-text">
+            <b>weight : </b>
+            {pet["weight"]["imperial"]} imperial
+          </p>
+          <p className="card-text">
+            <b>temperament : </b>
+            {pet["temperament"]}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
